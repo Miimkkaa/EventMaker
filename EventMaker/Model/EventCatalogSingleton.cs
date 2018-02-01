3bf7ff903b62f8f91b87dded362342c3158318f8
Benjamin Agic <benj769b@edu.easj.dk>
2018-02-01 11:05:00 +0100

    Filled classes eventhandler,EventCatalogSingleton,eventVM

--- EventMaker/Model/EventCatalogSingleton.cs
+++ EventMaker/Model/EventCatalogSingleton.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,26 @@ namespace EventMaker.Model
 {
     class EventCatalogSingleton
     {
+
+        //public EventCatalogSingleton 
+        public ObservableCollection<Event> Events { get; set; }
+
+
+
+
+        public void Add(Event newEvent)
+        {
+            
+        }
+
+        public void LoadEventAsync()
+        {
+            
+        }
+
+        public void Remove(Event eventToBeRemoved)
+        {
+            
+        }
     }
 }
--- EventMaker/ViewModel/EventViewModel.cs
+++ EventMaker/ViewModel/EventViewModel.cs
@@ -3,10 +3,64 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using EventMaker.Common;
+using EventMaker.Model;
 
 namespace EventMaker.ViewModel
 {
     class EventViewModel
     {
+        public DateTimeOffset Date;
+        public Event SelectedEvent;
+        public EventCatalogSingleton SingletonEvent;
+        public TimeSpan Time;
+        public RelayCommand CreateEvent { get; set; }
+        public RelayCommand DeleteEvent{ get; set; }
+        public RelayCommand SelectEvent { get; set; }
+
+        private int _id;
+        private string _description;
+        private string _name;
+        private string _place;
+
+
+        public int ID
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public string Description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public string Place
+        {
+            get { return _place; }
+            set { _place = value; }
+        }
+        
+
+
+        public EventViewModel(int id, string description, string name, string place)
+        {
+            _id = id;
+            _description = description;
+            _name = name;
+            _place = place;
+        }
+
+
+
+
     }
 }
