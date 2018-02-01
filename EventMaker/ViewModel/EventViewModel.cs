b914fb53978186f8ad76ba3d0144e68b6a9ac742
Izabela Kuzniar <izab0583@edu.easj.dk>
2018-02-01 13:03:02 +0100

    some changes

--- EventMaker/ViewModel/EventViewModel.cs
+++ EventMaker/ViewModel/EventViewModel.cs
@@ -11,20 +11,21 @@ namespace EventMaker.ViewModel
 {
     class EventViewModel
     {
-        public DateTimeOffset Date;
-        public Event SelectedEvent;
-        public EventCatalogSingleton SingletonEvent;
-        public TimeSpan Time;
-        public RelayCommand CreateEvent { get; set; }
-        public RelayCommand DeleteEvent{ get; set; }
-        public RelayCommand SelectEvent { get; set; }
-
+        //instance fields
         private int _id;
         private string _description;
         private string _name;
         private string _place;
 
-
+        //props
+        public DateTimeOffset Date { get; set; }
+        public Event SelectedEvent { get; set; }
+        public EventCatalogSingleton SingletonEvent { get; set; }
+        public TimeSpan Time { get; set; }
+        public RelayCommand CreateEvent { get; set; }
+        public RelayCommand DeleteEvent{ get; set; }
+        public RelayCommand SelectEvent { get; set; }
+        
         public int ID
         {
             get { return _id; }
@@ -48,8 +49,6 @@ namespace EventMaker.ViewModel
             get { return _place; }
             set { _place = value; }
         }
-        
-
 
         public EventViewModel(int id, string description, string name, string place)
         {
@@ -58,9 +57,7 @@ namespace EventMaker.ViewModel
             _name = name;
             _place = place;
         }
-
-
-
+        
 
     }
 }
