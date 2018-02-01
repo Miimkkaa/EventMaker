f89044ebbff732e638332a2199d1f3bf7e12d3b3
Anina <anin0543>
2018-02-01 11:19:32 +0100

    added some changes

--- EventMaker/Convertor/DataTimeConvertor.cs
+++ EventMaker/Convertor/DataTimeConvertor.cs
@@ -8,5 +8,12 @@ namespace EventMaker.Convertor
 {
     class DataTimeConvertor
     {
+        public static DateTime DateTimeOffsetAndTimeSetToDateTime(DateTimeOffset date, TimeSpan time)
+        {
+            return new DateTime(date.Year, date.Month, date.Day, time.Hours, time.Minutes, 0);
+        }
+
+
+
     }
 }
