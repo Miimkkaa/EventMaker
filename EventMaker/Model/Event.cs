55d209139b366f1a89260036c2d27334a839a0bd
Milena Ognyanova <mile0416@edu.easj.dk>
2018-02-01 10:56:45 +0100

    Added Modelclass Event

--- EventMaker/Model/Event.cs
+++ EventMaker/Model/Event.cs
@@ -8,5 +8,31 @@ namespace EventMaker.Model
 {
     class Event
     {
+        private string _name;
+        private string _type;
+        private string _description;
+        private DateTime _dateTime;
+        private string _location;
+
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Description { get; set; }
+        public DateTime DateTime { get; set; }
+        public string Location { get; set; }
+
+        public Event( string name, string type, string description, string datetime, string location)
+        {
+            _name = name;
+            _type = type;
+            _description = description;
+            _datetime = datetime;
+            _location = location;
+        }
+
+        public override string Tostring()
+        {
+            return $"{Name} {Type} {Description} {DateTime} {Location}";
+        }
     }
+
 }
